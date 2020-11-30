a435ed465aa3ed2904872e029cc5ab7f9a95a20b
wanderer120 <megamanx120@hotmail.com>
2020-11-30 19:20:00 +0800

    test gh page

--- src/App.js
+++ src/App.js
@@ -92,80 +92,81 @@ class App extends Component {
     if(window.ethereum!==undefined){
       this.setState({isW3:true});
     }
-    window.ethereum.on('accountsChanged', function (accounts) {
-      //window.location.reload();
+    if(window.ethereum!==undefined){
+      window.ethereum.on('accountsChanged', function (accounts) {
+        //window.location.reload();
 
-      if (!ethEnabled()) {
-        alert("Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!");
-      }
-      else{
-        let userDetailObj = {};
-        window.web3.eth.getAccounts().then((accounts)=>{
-          if (accounts.length === 0) {
-            console.log('MetaMask is locked')
-            app.setState({isLogin:false})
-          }
-          else {
-            app.setState({isLogin:true})
-            userDetailObj.account = accounts[0]
-            window.web3.eth.getBalance(accounts[0]).then((value)=>{
-              userDetailObj.balance = window.web3.utils.fromWei(value)
-              app.setState({userDetail:userDetailObj})
-            });
-            userDetailObj.contract = new window.web3.eth.Contract(_ABI, _CONTRACT_ADDRESS)
-            userDetailObj.contract.methods.isUserExists(userDetailObj.account).call().then((result)=>{
-              userDetailObj.userExist = result
-              app.setState({userExist:result})
-            });
-            userDetailObj.contract.methods.users(userDetailObj.account).call().then((result)=>{
-              userDetailObj.itemCount = result.itemCount;
-              app.setState({itemCount:result.itemCount})
-            });
-            userDetailObj.contract.methods.lastItemId().call().then((result)=>{
-              app.setState({universeItemCount:(result)})
-            });
-            userDetailObj.contract.methods.getAllItemsByUser(userDetailObj.account).call().then((result)=>{
-              let itemObj = {};
-              itemObj.idArr = [];
-              itemObj.powerArr = [];
-              itemObj.lastWinArr = [];
-              for(let i=0;i<result[Object.keys(result)[0]].length;i++){
-                itemObj.idArr[i] = result[Object.keys(result)[0]][i];
-                itemObj.powerArr[i] = result[Object.keys(result)[1]][i];
-                itemObj.lastWinArr[i] = window.web3.utils.fromWei(result[Object.keys(result)[2]][i],'ether');
-              }
-              userDetailObj.itemIdArr = itemObj.idArr;
-              userDetailObj.itemPowerArr = itemObj.powerArr;
-              userDetailObj.lastWinArr = itemObj.lastWinArr;
-              app.setState({userDetail:userDetailObj});
-            });
-            userDetailObj.contract.methods.lastItemId().call().then((result)=>{
-              if(result>8){
-                userDetailObj.winnerArr = [];
-                userDetailObj.winnerAmountArr = [];
-                for(let i=0;i<8;i++){
-                  userDetailObj.contract.methods.allItems(i).call().then((result)=>{
-                    userDetailObj.winnerArr[i] = result.owner.substring(0,6)+"...";
-                    userDetailObj.winnerAmountArr[i] = window.web3.utils.fromWei(result.LastWinAmount,'ether');
-                    app.setState({userDetail:userDetailObj});
-                  });
+        if (!ethEnabled()) {
+          alert("Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!");
+        }
+        else{
+          let userDetailObj = {};
+          window.web3.eth.getAccounts().then((accounts)=>{
+            if (accounts.length === 0) {
+              console.log('MetaMask is locked')
+              app.setState({isLogin:false})
+            }
+            else {
+              app.setState({isLogin:true})
+              userDetailObj.account = accounts[0]
+              window.web3.eth.getBalance(accounts[0]).then((value)=>{
+                userDetailObj.balance = window.web3.utils.fromWei(value)
+                app.setState({userDetail:userDetailObj})
+              });
+              userDetailObj.contract = new window.web3.eth.Contract(_ABI, _CONTRACT_ADDRESS)
+              userDetailObj.contract.methods.isUserExists(userDetailObj.account).call().then((result)=>{
+                userDetailObj.userExist = result
+                app.setState({userExist:result})
+              });
+              userDetailObj.contract.methods.users(userDetailObj.account).call().then((result)=>{
+                userDetailObj.itemCount = result.itemCount;
+                app.setState({itemCount:result.itemCount})
+              });
+              userDetailObj.contract.methods.lastItemId().call().then((result)=>{
+                app.setState({universeItemCount:(result)})
+              });
+              userDetailObj.contract.methods.getAllItemsByUser(userDetailObj.account).call().then((result)=>{
+                let itemObj = {};
+                itemObj.idArr = [];
+                itemObj.powerArr = [];
+                itemObj.lastWinArr = [];
+                for(let i=0;i<result[Object.keys(result)[0]].length;i++){
+                  itemObj.idArr[i] = result[Object.keys(result)[0]][i];
+                  itemObj.powerArr[i] = result[Object.keys(result)[1]][i];
+                  itemObj.lastWinArr[i] = window.web3.utils.fromWei(result[Object.keys(result)[2]][i],'ether');
                 }
-              }
-            });
-          }
-        },()=>{
-          console.log("fail callback");
-        })
-      }
-    });
+                userDetailObj.itemIdArr = itemObj.idArr;
+                userDetailObj.itemPowerArr = itemObj.powerArr;
+                userDetailObj.lastWinArr = itemObj.lastWinArr;
+                app.setState({userDetail:userDetailObj});
+              });
+              userDetailObj.contract.methods.lastItemId().call().then((result)=>{
+                if(result>8){
+                  userDetailObj.winnerArr = [];
+                  userDetailObj.winnerAmountArr = [];
+                  for(let i=0;i<8;i++){
+                    userDetailObj.contract.methods.allItems(i).call().then((result)=>{
+                      userDetailObj.winnerArr[i] = result.owner.substring(0,6)+"...";
+                      userDetailObj.winnerAmountArr[i] = window.web3.utils.fromWei(result.LastWinAmount,'ether');
+                      app.setState({userDetail:userDetailObj});
+                    });
+                  }
+                }
+              });
+            }
+          },()=>{
+            console.log("fail callback");
+          })
+        }
+      });
+    }
   }
   render() {
     if(this.state.isLogin && this.state.isW3){
       return (
         <MainPage userDetail={this.state.userDetail} universeItemCount={this.state.universeItemCount}/>
       );
-    }
-    else {
+    }else {
       return (
         <div className="container" style={{display: 'flex', justifyContent: 'center'}}>
           <Button variant="primary" onClick={this.LoginToMetamask}>Connect to Metamask</Button>
